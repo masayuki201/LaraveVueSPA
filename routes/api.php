98c6e0506d13ae3b25daa664a95da583d170c040
m-ono <m-ono@advanced-media.co.jp>
2022-01-25 13:16:21 +0900

    feat:タスク登録API実装

--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,5 @@ use Illuminate\Http\Request;
 */
 
 Route::get('/tasks', 'TaskController@index');
+Route::post('/tasks', 'TaskController@store');
 Route::get('/tasks/{task}', 'TaskController@show');
