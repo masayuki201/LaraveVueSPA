28b9dc23089971e7650445e7ebc30f23c293df8e
m-ono <m-ono@advanced-media.co.jp>
2022-01-26 17:08:10 +0900

    feat:タスク詳細ページのカード化

--- resources/js/components/TaskListComponent.vue
+++ resources/js/components/TaskListComponent.vue
@@ -1,59 +1,61 @@
 <template>
     <v-container>
-        <v-card
-            class="table table-hover"
-            max-width="639"
-        >
-            <thead class="thead-light">
-                <tr>
-                    <th scope="col">No</th>
-                    <th scope="col">タイトル</th>
-                    <th scope="col">内容</th>
-                    <th scope="col">担当者</th>
-                    <th scope="col">詳細</th>
-                    <th scope="col">編集</th>
-                    <th scope="col">削除</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(task, index) in tasks" :key="indnex">
-                   <th scope="row">{{ task.id }}</th>
-                   <td>{{ task.title }}</td>
-                   <td>{{ task.content }}</td>
-                   <td> {{ task.person_in_charge }}</td>
-                   <td>
-                       <v-btn
-                           class="showBtn"
-                           color="#00B0FF"
-                           elevation="10"
-                           :to="{name: 'task.show', params: {taskId: task.id }}"
-                       >
-                           詳細
-                       </v-btn>
-                   </td>
-                   <td>
-                       <v-btn
-                           class="editBtn"
-                           color="#7CB342"
-                           elevation="10"
-                           :to="{name: 'task.edit', params: {taskId: task.id }}"
-                       >
-                           編集
-                       </v-btn>
-                   </td>
-                   <td>
-                       <v-btn
-                           class="deleteBtn"
-                           color="#F06292"
-                           elevation="10"
-                           @click="deleteTask(task.id)"
-                       >
-                           削除
-                       </v-btn>
-                   </td>
-                </tr>
-            </tbody>
-        </v-card>
+        <div class="justify-content:center">
+            <v-card
+                class="table table-hover"
+                max-width="639"
+            >
+                <thead class="thead-light">
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">タイトル</th>
+                        <th scope="col">内容</th>
+                        <th scope="col">担当者</th>
+                        <th scope="col">詳細</th>
+                        <th scope="col">編集</th>
+                        <th scope="col">削除</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(task, index) in tasks" :key="indnex">
+                       <th scope="row">{{ task.id }}</th>
+                       <td>{{ task.title }}</td>
+                       <td>{{ task.content }}</td>
+                       <td> {{ task.person_in_charge }}</td>
+                       <td>
+                           <v-btn
+                               class="showBtn"
+                               color="#00B0FF"
+                               elevation="10"
+                               :to="{name: 'task.show', params: {taskId: task.id }}"
+                           >
+                               詳細
+                           </v-btn>
+                       </td>
+                       <td>
+                           <v-btn
+                               class="editBtn"
+                               color="#7CB342"
+                               elevation="10"
+                               :to="{name: 'task.edit', params: {taskId: task.id }}"
+                           >
+                               編集
+                           </v-btn>
+                       </td>
+                       <td>
+                           <v-btn
+                               class="deleteBtn"
+                               color="#F06292"
+                               elevation="10"
+                               @click="deleteTask(task.id)"
+                           >
+                               削除
+                           </v-btn>
+                       </td>
+                    </tr>
+                </tbody>
+            </v-card>
+        </div>
     </v-container>
 </template>
 
