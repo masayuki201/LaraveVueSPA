73e4a646a9e108e9b3fcd31ccde252cef28affcc
m-ono <m-ono@advanced-media.co.jp>
2022-01-26 14:51:37 +0900

    feat:ヘッダーにvuetify使用

--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -1,22 +1,43 @@
 <template>
-    <div class="container-fluid bg-dark mb-3">
-        <div class="container">
-            <nav class="navbar navbar-dark">
-                <span class="navbar-brand mb-0 h1">Task管理アプリ</span>
-                <div>
-                <router-link v-bind:to="{name: 'task.list'}">
-                    <button class="btn btn-success">一覧</button>
-                </router-link>
-                <router-link v-bind:to="{name: 'task.create'}">
-                    <button class="btn btn-success">追加する</button>
-                </router-link>
-                </div>
-            </nav>
-        </div>
-    </div>
+    <v-card>
+        <v-toolbar
+            class="text-center"
+            cols="12"
+            color="blue lighten-1"
+        >
+            <h2
+                class="white--text"
+            >
+                Task管理アプリ
+            </h2>
+            <v-spacer></v-spacer>
+            <v-btn
+                class="btn white--text"
+                color="#F57C00"
+                elevation="10"
+                :to="{name: 'task.list'}"
+            >
+                一覧
+            </v-btn>
+            <v-btn
+                class="btn white--text"
+                color="#F57C00"
+                elevation="10"
+                :to="{name: 'task.create'}"
+            >
+                追加する
+            </v-btn>
+        </v-toolbar>
+    </v-card>
 </template>
 <script>
 export default {
     name: "HeaderComponent"
 }
 </script>
+
+<style>
+.btn{
+    margin: 0px 5px 0px 5px;
+}
+</style>
