91c4c8266bd0b8fbd77710ea56c6e2e740913fed
masayuki201 <masayuki201_201@yahoo.co.jp>
2022-01-27 00:08:31 +0900

    feat:各ページの横スクロールバー追加

--- resources/js/components/TaskCreateComponent.vue
+++ resources/js/components/TaskCreateComponent.vue
@@ -1,20 +1,26 @@
 <template>
-    <v-container>
-        <div class="justify-content-center">
-            <form v-on:submit.prevent="submit">
-                <v-card class="col-sm-6">
-                    <div class="form-group row">
-                        <label for="title" class="col-sm-3 col-form-label">タイトル</label>
-                        <input type="text" class="col-sm-9 form-control" id="title" v-model="task.title">
-                    </div>
-                    <div class="form-group row">
-                        <label for="title" class="col-sm-3 col-form-label">内容</label>
-                        <input type="text" class="col-sm-9 form-control" id="content" v-model="task.content">
-                    </div>
-                    <div class="form-group row">
-                        <label for="title" class="col-sm-3 col-form-label">担当者</label>
-                        <input type="text" class="col-sm-9 form-control" id="person-in-charge" v-model="task.person_in_charge">
-                    </div>
+    <v-list
+        class="overflow-x-auto mx-auto my-5"
+        width="100%"
+        max-width="2000"
+    >
+        <v-container>
+            <div class="justify-content-center">
+                <form v-on:submit.prevent="submit">
+                    <v-card class="col-sm-6">
+                        <div class="form-group row">
+                            <label for="title" class="col-sm-3 col-form-label">タイトル</label>
+                            <input type="text" class="col-sm-9 form-control" id="title" v-model="task.title">
+                        </div>
+                        <div class="form-group row">
+                            <label for="title" class="col-sm-3 col-form-label">内容</label>
+                            <input type="text" class="col-sm-9 form-control" id="content" v-model="task.content">
+                        </div>
+                        <div class="form-group row">
+                            <label for="title" class="col-sm-3 col-form-label">担当者</label>
+                            <input type="text" class="col-sm-9 form-control" id="person-in-charge" v-model="task.person_in_charge">
+                        </div>
+                    </v-card>
                     <v-btn
                         type="submit"
                         class="btn white--text"
@@ -23,10 +29,10 @@
                     >
                         追加する
                     </v-btn>
-                </v-card>
-            </form>
-        </div>
-    </v-container>
+                </form>
+            </div>
+        </v-container>
+    </v-list>
 </template>
 
 <script>
